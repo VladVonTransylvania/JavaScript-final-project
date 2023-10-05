d15a40bd9dbcfc406280c8588a31c888fe4d77de
VladVonTransylvania <105380245+VladVonTransylvania@users.noreply.github.com>
2023-10-05 20:29:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,62 +2,84 @@
 
 const mockData = require('./mockData.js').data;
 const userProfile = {};
-const prompt = require('prompt-sync')();
+let matchCount = 0;
 
-let userInput, matchCount = 0;
-
+// Display a welcome message to the user
 console.log('Welcome to the Dating App!');
+
+// Prompt the user to provide their details for finding potential matches
 console.log('Please provide your details to find potential matches.');
 
-// Validate and set first name
-userInput = '';
-while (userInput.trim() === '') {
-  userInput = prompt('What is your first name? ');
+// Prompt and validate first name
+userProfile.first_name = '';
+while (userProfile.first_name.trim() === '') {
+  userProfile.first_name = prompt('What is your first name? ');
 }
-userProfile.first_name = userInput.trim();
 
-// Validate and set last name
-userInput = '';
-while (userInput.trim() === '') {
-  userInput = prompt('What is your last name? ');
+// Prompt and validate last name
+userProfile.last_name = '';
+while (userProfile.last_name.trim() === '') {
+  userProfile.last_name = prompt('What is your last name? ');
 }
-userProfile.last_name = userInput.trim();
 
-userInput = prompt('What is your age? ');
-userProfile.age = !isNaN(userInput) && parseInt(userInput) >= 18 ? parseInt(userInput) : -1;
+// Prompt and validate age
+userProfile.age = -1;
+while (isNaN(userProfile.age) || userProfile.age < 18) {
+  const userInput = prompt('What is your age? ');
+  userProfile.age = parseInt(userInput);
+}
 
-userInput = prompt('What is your minimum interested age? ');
-userProfile.min_age_interest = !isNaN(userInput) && parseInt(userInput) >= 18 ? parseInt(userInput) : -1;
+// Prompt and validate minimum interested age
+userProfile.min_age_interest = -1;
+while (isNaN(userProfile.min_age_interest) || userProfile.min_age_interest < 18) {
+  const userInput = prompt('What is your minimum interested age? ');
+  userProfile.min_age_interest = parseInt(userInput);
+}
 
-userInput = prompt('What is your maximum interested age? ');
-userProfile.max_age_interest = !isNaN(userInput) && parseInt(userInput) >= 18 ? parseInt(userInput) : -1;
+// Prompt and validate maximum interested age
+userProfile.max_age_interest = -1;
+while (isNaN(userProfile.max_age_interest) || userProfile.max_age_interest < 18 || userProfile.max_age_interest < userProfile.min_age_interest) {
+  const userInput = prompt('What is your maximum interested age? ');
+  userProfile.max_age_interest = parseInt(userInput);
+}
 
-userInput = prompt('What is your gender? (M/F/X) ');
-userProfile.gender = ['M', 'F', 'X'].includes(userInput.toUpperCase()) ? userInput.toUpperCase() : '';
+// Prompt and validate gender
+userProfile.gender = '';
+while (!['M', 'F', 'X'].includes(userProfile.gender.toUpperCase())) {
+  userProfile.gender = prompt('What is your gender? (M/F/X) ').toUpperCase();
+}
 
-userInput = prompt('What gender are you interested in dating? (M/F/X) ');
-userProfile.gender_interest = ['M', 'F', 'X'].includes(userInput.toUpperCase()) ? userInput.toUpperCase() : '';
+// Prompt and validate gender interest
+userProfile.gender_interest = '';
+while (!['M', 'F', 'X'].includes(userProfile.gender_interest.toUpperCase())) {
+  userProfile.gender_interest = prompt('What gender are you interested in dating? (M/F/X) ').toUpperCase();
+}
 
-userInput = prompt('What is your location? (rural/city) ');
-userProfile.location = ['rural', 'city'].includes(userInput.toLowerCase()) ? userInput.toLowerCase() : '';
+// Prompt and validate location
+userProfile.location = '';
+while (!['rural', 'city'].includes(userProfile.location.toLowerCase())) {
+  userProfile.location = prompt('What is your location? (rural/city) ').toLowerCase();
+}
 
+// Iterate through mockData and count matches
 for (const candidate of mockData) {
   const { age: candidateAge, min_age_interest: candidateMinAgeInterest, max_age_interest: candidateMaxAgeInterest,
     gender: candidateGender, gender_interest: candidateGenderInterest, location: candidateLocation } = candidate;
 
-  if ((userProfile.age >= candidateMinAgeInterest && userProfile.age <= candidateMaxAgeInterest) ||
-      (candidateAge >= userProfile.min_age_interest && candidateAge <= userProfile.max_age_interest)) {
-    if (userProfile.gender_interest === candidateGender || candidateGenderInterest === userProfile.gender) {
-      if (userProfile.location === candidateLocation) {
-        matchCount++;
-        console.log(`Match found: ${candidate.first_name} ${candidate.last_name} (Age: ${candidateAge}, Location: ${candidateLocation})`);
+  // Check if the candidate matches the user's criteria
+  if (userProfile.age >= candidateMinAgeInterest && userProfile.age <= candidateMaxAgeInterest) {
+    if (candidateAge >= userProfile.min_age_interest && candidateAge <= userProfile.max_age_interest) {
+      if (userProfile.gender_interest === candidateGender || candidateGenderInterest === userProfile.gender) {
+        if (userProfile.location === candidateLocation) {
+          matchCount++;
+          console.log(`Match found: ${candidate.first_name} ${candidate.last_name} (Age: ${candidateAge}, Location: ${candidateLocation})`);
+        }
       }
     }
   }
 }
 
 console.log(`Total Matches: ${matchCount}`);
-
 
 
 
