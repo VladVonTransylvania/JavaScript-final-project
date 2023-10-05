005db929dbfddf8cd2a5acab52c2223189a3fbae
VladVonTransylvania <105380245+VladVonTransylvania@users.noreply.github.com>
2023-10-05 17:14:37 +0000

    Second version

--- index.js
+++ index.js
@@ -9,32 +9,37 @@ let userInput, matchCount = 0;
 console.log('Welcome to the Dating App!');
 console.log('Please provide your details to find potential matches.');
 
+// Validate and set first name
+userInput = '';
+while (userInput.trim() === '') {
+  userInput = prompt('What is your first name? ');
+}
+userProfile.first_name = userInput.trim();
 
+// Validate and set last name
+userInput = '';
+while (userInput.trim() === '') {
+  userInput = prompt('What is your last name? ');
+}
+userProfile.last_name = userInput.trim();
 
-const validateInput = (input, validationFn) => {
-  userInput = prompt(input);
-  while (!validationFn(userInput)) {
-    console.log('Invalid input. Please try again.');
-    userInput = prompt(input);
-  }
-  return userInput;
-};
+userInput = prompt('What is your age? ');
+userProfile.age = !isNaN(userInput) && parseInt(userInput) >= 18 ? parseInt(userInput) : -1;
 
-const validateName = (name) => /^[a-zA-Z]+$/.test(name) && name.length > 0;
-const validateNumber = (num) => !isNaN(num);
-const validateAge = (age) => age >= 18;
-const validateAgeRange = (minAge, maxAge) => minAge >= 18 && maxAge >= 18 && maxAge > minAge;
-const validateGender = (gender) => ['M', 'F', 'X'].includes(gender.toUpperCase());
-const validateLocation = (location) => ['rural', 'city'].includes(location.toLowerCase());
+userInput = prompt('What is your minimum interested age? ');
+userProfile.min_age_interest = !isNaN(userInput) && parseInt(userInput) >= 18 ? parseInt(userInput) : -1;
 
-userProfile.first_name = validateInput('What is your first name? ', validateName);
-userProfile.last_name = validateInput('What is your last name? ', validateName);
-userProfile.age = Number(validateInput('What is your age? ', validateNumber));
-userProfile.min_age_interest = Number(validateInput('What is your minimum interested age? ', validateNumber));
-userProfile.max_age_interest = Number(validateInput('What is your maximum interested age? ', validateNumber));
-userProfile.gender = validateInput('What is your gender? (M/F/X) ', validateGender).toUpperCase();
-userProfile.gender_interest = validateInput('What gender are you interested in dating? (M/F/X) ', validateGender).toUpperCase();
-userProfile.location = validateInput('What is your location? (rural/city) ', validateLocation).toLowerCase();
+userInput = prompt('What is your maximum interested age? ');
+userProfile.max_age_interest = !isNaN(userInput) && parseInt(userInput) >= 18 ? parseInt(userInput) : -1;
+
+userInput = prompt('What is your gender? (M/F/X) ');
+userProfile.gender = ['M', 'F', 'X'].includes(userInput.toUpperCase()) ? userInput.toUpperCase() : '';
+
+userInput = prompt('What gender are you interested in dating? (M/F/X) ');
+userProfile.gender_interest = ['M', 'F', 'X'].includes(userInput.toUpperCase()) ? userInput.toUpperCase() : '';
+
+userInput = prompt('What is your location? (rural/city) ');
+userProfile.location = ['rural', 'city'].includes(userInput.toLowerCase()) ? userInput.toLowerCase() : '';
 
 for (const candidate of mockData) {
   const { age: candidateAge, min_age_interest: candidateMinAgeInterest, max_age_interest: candidateMaxAgeInterest,
@@ -52,4 +57,11 @@ for (const candidate of mockData) {
 }
 
 console.log(`Total Matches: ${matchCount}`);
+
+
+
+
+
+
+
 
